a8082fd76c28401b309d41d7a476dcd509e9dfdf
mohammadibrahim20 <ibinfo2017bd@gmail.com>
2023-09-07 00:29:11 +0600

    Add Box Done

---
+++ src/Page/Box/GenaralInfo.jsx
@@ -0,0 +1,28 @@
+import { motion } from "framer-motion";
+import { AiOutlineProfile } from "react-icons/ai";
+import { GiEnlightenment } from "react-icons/gi";
+
+import Title from "../../components/Title";
+const GenaralInfo = () => {
+  return (
+    <motion.circle
+    cx={500} animate={{ cx: [null, 100] }}
+    >
+    <div className=" border-4 drop-shadow-sm px-10 py-5 rounded-2xl bg-yellow-50 hover:bg-yellow-500 hover:text-white transform transition duration-500 hover:scale-105 ">
+      <Title title="General Information" />
+      <div className="flex gap-2 mt-5">
+        <GiEnlightenment className="hover:text-black" size={100} />
+        <div className="space-y-3">
+            <span className="flex justify-center items-center gap-2"><AiOutlineProfile/>সরকারি সিটি কলেজ সৃষ্টির ইতিহাস</span>
+            <span className="flex justify-center items-center gap-2"><AiOutlineProfile/>সরকারি সিটি কলেজ সৃষ্টির ইতিহাস</span>
+            <span className="flex justify-center items-center gap-2"><AiOutlineProfile/>সরকারি সিটি কলেজ সৃষ্টির ইতিহাস</span>
+            <span className="flex justify-center items-center gap-2"><AiOutlineProfile/>সরকারি সিটি কলেজ সৃষ্টির ইতিহাস</span>
+            <span className="flex justify-center items-center gap-2"><AiOutlineProfile/>সরকারি সিটি কলেজ সৃষ্টির ইতিহাস</span>
+        </div>
+      </div>
+    </div>
+    </motion.circle>
+  );
+};
+
+export default GenaralInfo;
--- src/Page/Home/LeftSide.jsx
+++ src/Page/Home/LeftSide.jsx
@@ -1,15 +1,22 @@
-import AboutCollege from "./AboutCollege"
-import Notice from "./Notice"
-
+import GenaralInfo from "../Box/GenaralInfo";
+import AboutCollege from "./AboutCollege";
+import Notice from "./Notice";
 
 const LeftSide = () => {
   return (
     <>
       <AboutCollege />
       <Notice />
+      <div className="grid md:grid-cols-2 gap-5 my-10">
+        <GenaralInfo />
+        <GenaralInfo />
+        <GenaralInfo />
+        <GenaralInfo />
+        <GenaralInfo />
+        <GenaralInfo />
+      </div>
     </>
+  );
+};
 
-  )
-}
-
-export default LeftSide
+export default LeftSide;
